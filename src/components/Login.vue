4db44a8d1a129cd799d7c58108ecc309eace0645
coderwyx <2514138002@qq.com>
2021-07-08 01:07:01 +0800

    项目初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> 
+    登录组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data () {
+   return {}
+  },
+   methods:{}
+}
+</script>
+
+<style scoped>
+
+</style>
